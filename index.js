42c70886d0d1304ed04ac73c15189af70f1a2e26
Mahedi-432A <mdmahedih038@gmail.com>
2025-11-12 03:43:50 +0600

    Refactor code style for consistency and readability in index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
-// middlewares 
+// middlewares
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@taskmanager.rhmpuoe.mongodb.net/?appName=TaskManager`;
 
@@ -18,58 +17,74 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const jobsCollection = client.db('jobPortal').collection('jobs');
-    const applicationsCollection = client.db('jobPortal').collection('applications');
+    const jobsCollection = client.db("jobPortal").collection("jobs");
+    const applicationsCollection = client
+      .db("jobPortal")
+      .collection("applications");
 
     // jobs api
-    app.get('/jobs', async (req, res) => {
-        const cursor = jobsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get("/jobs", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+
+      if (email) {
+        query.hr_email = email;
+      }
+
+      const cursor = jobsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
-    app.get('/jobs/:id', async (req, res) => {
+    // could be used for recruiters posted jobs /--- But should not be used /----/...
+    // app.get('/jobsPostedByMe', async ( req, res ) => {
+    //   const email = req.query.email;
+    //   const query = { hr_email: email };
+    //   const result = await jobsCollection.find(query).toArray();
+    //   res.send(result);
+    // });
+
+    app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id : new ObjectId(id) }
+      const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
       res.send(result);
     });
 
-    app.post('/jobs', async ( req, res ) => {
+    app.post("/jobs", async (req, res) => {
       const addJob = req.body;
       const result = await jobsCollection.insertOne(addJob);
       res.send(result);
-    })
+    });
 
     // job applicaion related api
-    app.get('/applications', async ( req, res ) => {
+    app.get("/applications", async (req, res) => {
       const email = req.query.email;
-      const query = { applicant: email};
+      const query = { applicant: email };
       const result = await applicationsCollection.find(query).toArray();
 
       // experiment /- bad way to aggregate data/- -/.
-      for( const application of result){
+      for (const application of result) {
         const jobId = application.jobId;
-        const jobQuery = { _id: new ObjectId(jobId)};
+        const jobQuery = { _id: new ObjectId(jobId) };
         const job = await jobsCollection.findOne(jobQuery);
-        application.company = job.company
-        application.title = job.title
-        application.company_logo = job.company_logo
+        application.company = job.company;
+        application.title = job.title;
+        application.company_logo = job.company_logo;
       }
       //-----------
       res.send(result);
     });
 
-    app.post ('/applications', async( req, res ) => {
+    app.post("/applications", async (req, res) => {
       const application = req.body;
       const result = await applicationsCollection.insertOne(application);
       res.send(result);
@@ -77,7 +92,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -85,12 +102,11 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 // initial route
-app.get('/', (req, res) => {
-    res.send('server is running');
-})
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
 app.listen(port, () => {
-    console.log(`this app is running on port ${port}`);
-})
+  console.log(`this app is running on port ${port}`);
+});
